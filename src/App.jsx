25d336a9d15c06bbf192e81d02616e94078c4323
78Pavel78 <112005061+78Pavel78@users.noreply.github.com>
2022-11-30 18:53:29 +0300

    Add files via upload
    пропсы

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+import Students from './components/students.jsx'
+
+function App()  {
+    return (
+    <div className="App">  
+    <Students />
+    </div>
+    );
+}
+
+export default App;
+
